d2cc9ee9e82a95f072e27eb52cca26a263bea7cb
Linus Phan <phanlinus@gmail.com>
2019-08-16 19:48:49 -0700

    add categories and update videos index view

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_235420) do
+ActiveRecord::Schema.define(version: 2019_08_17_023311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "videos", force: :cascade do |t|
     t.string "title"
@@ -22,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_08_16_235420) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
+    t.integer "category_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,31 @@
+Category.create(name: 'Comedy')
+Category.create(name: 'Drama')
+
 Video.create(
   title: 'Family Guy',
   description: 'This is Family Guy.',
   small_cover_url: '/tmp/family_guy.jpg',
-  large_cover_url: '/tmp/family_guy.jpg'
+  large_cover_url: '/tmp/family_guy.jpg',
+  category: Category.first
 )
 Video.create(
   title: 'Monk',
   description: 'This is Monk.',
   small_cover_url: '/tmp/monk.jpg',
-  large_cover_url: '/tmp/monk_large.jpg'
+  large_cover_url: '/tmp/monk_large.jpg',
+  category: Category.first
 )
 Video.create(
   title: 'Futurama',
   description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.',
   small_cover_url: '/tmp/futurama.jpg',
-  large_cover_url: '/tmp/futurama.jpg'
+  large_cover_url: '/tmp/futurama.jpg',
+  category: Category.last
 )
 Video.create(
   title: 'South Park',
   description: 'This is South Park.',
   small_cover_url: '/tmp/south_park.jpg',
-  large_cover_url: '/tmp/south_park.jpg'
+  large_cover_url: '/tmp/south_park.jpg',
+  category: Category.first
 )
