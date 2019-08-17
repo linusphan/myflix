a65e30780976d5b2eafd971b0eeb58613b55a580
Linus Phan <phanlinus@gmail.com>
2019-08-16 23:43:31 -0700

    add show categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
-  root to: 'videos#index'
+  root         to: 'videos#index'
   get '/home', to: 'videos#index'
 
-  resources :videos, only: [:index, :show]
+  resources :videos,     only: [:index, :show]
+  resources :categories, only: [:show]
 end
