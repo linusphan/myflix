017784d66fb15008f728cf52bcae132dbe426258
Linus Phan <phanlinus@gmail.com>
2019-08-17 14:16:11 -0700

    add video model validations and test validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,4 @@
 class Video < ApplicationRecord
   belongs_to :category
+  validates_presence_of :title, :description
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -16,4 +16,14 @@ describe Video do
                         category: dramas)
     expect(monk.category).to eq(dramas)
   end
+
+  it 'does not save a video without a title' do
+    video = Video.create(description: 'a great video!')
+    expect(Video.count).to eq(0)
+  end
+
+  it 'does not save a video without a description' do
+    video = Video.create(title: 'monk')
+    expect(Video.count).to eq(0)
+  end
 end
