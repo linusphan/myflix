4463d7a96deec49035942f4315b309788ba396b6
Linus Phan <phanlinus@gmail.com>
2019-08-16 18:20:32 -0700

    add video model, controller, seeds, routes, and show and index actions for video controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'ui(/:action)', controller: 'ui'
+
+  root to: 'videos#index'
+  get '/home', to: 'videos#index'
+
+  resources :videos, only: [:index, :show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Video.create(
+  title: 'Family Guy',
+  description: 'This is Family Guy.',
+  small_cover_url: '/tmp/family_guy.jpg',
+  large_cover_url: '/tmp/family_guy.jpg'
+)
+Video.create(
+  title: 'Monk',
+  description: 'This is Monk.',
+  small_cover_url: '/tmp/monk.jpg',
+  large_cover_url: '/tmp/monk_large.jpg'
+)
+Video.create(
+  title: 'Futurama',
+  description: 'Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.',
+  small_cover_url: '/tmp/futurama.jpg',
+  large_cover_url: '/tmp/futurama.jpg'
+)
